e68857a4cb5af42cc47487dadf4eaf74f7c6305d
udcoder <chatterjeeuddipan783@gmail.com>
2016-11-22 19:58:14 -0500

    Update preprocess.py
    Updated code. 3 columns exist with nan values.

--- preprocess.py
+++ preprocess.py
@@ -7,41 +7,44 @@ import pandas as pd
 
 
 
-#my_data = genfromtxt('Training_Data_2016/training_data_2016.csv', delimiter=',')
-#Data = np.asarray(my_data)
-#unique=np.unique(column(Data,2))
-#print(unique)
-
 Data = pd.read_csv('Training_Data_2016/training_data_2016.csv',low_memory = False)
+
+# Drop unneccessary rows #
+
 Data = Data.drop('Vehicle_Annual_Miles', 1)
 Data = Data.drop('Vehicle_Comprehensive_Coverage_Limit', 1)
-replacements = {
-   'Policy_Zip_Code_Garaging_Location': {
-      r'Unknown': '00000',}
-    'Vehicle_Miles_To_Work': {
-      r'-1': np.nan,}
-    'Policy_Zip_Code_Garaging_Location': {
-      r'Unknown': '00000',}
-    'Vehicle_Passive_Restraint': {
-      r'Unknown': 'Y',}
-    'Vehicle_Med_Pay_Limit': {
-      r'Unknown': np.nan,}
-    'Policy_Zip_Code_Garaging_Location': {
-      r'Unknown': '00000',}
+Data = Data.drop('Driver_Minimum_Age', 1)
+Data = Data.drop('Driver_Maximum_Age', 1)
+Data = Data.drop('EEA_PolicyYear', 1)
+Data = Data.drop('Vehicle_New_Cost_Amount', 1)
 
+# Clearing unneccessary rows #
 
+Data = Data[Data.EEA_Policy_Tenure != -1]
+Data = Data[Data.Vehicle_Symbol != -1]
+Data = Data[Data.Vehicle_Days_Per_Week_Driven != -1]
+Data = Data[Data.Vehicle_Anti_Theft_Device != 'Unknown']
 
+# Replace missing Data #
 
+Data['Policy_Zip_Code_Garaging_Location'] = Data['Policy_Zip_Code_Garaging_Location'].replace('Unknown', '00000')
+Data['Vehicle_Miles_To_Work'] = Data['Vehicle_Miles_To_Work'].replace('-1', np.nan)
+Data['Vehicle_Passive_Restraint'] = Data['Vehicle_Passive_Restraint'].replace('Unknown', 'Y')
+Data['EEA_Policy_Zip_Code_3'] = Data['EEA_Policy_Zip_Code_3'].replace('Unknown', '000')
+Data['Vehicle_Med_Pay_Limit'] = Data['Vehicle_Med_Pay_Limit'].replace('-1', np.nan)
+Data['Vehicle_Physical_Damage_Limit'] = Data['Vehicle_Physical_Damage_Limit'].replace('-1', np.nan)
+Data['Vehicle_Collision_Coverage_Deductible'] = Data['Vehicle_Collision_Coverage_Deductible'].replace('-1', np.nan)
 
+# Fill nan data #
 
-}
+Data['Vehicle_Miles_To_Work'].fillna((Data['Vehicle_Miles_To_Work'].mean()), inplace=True)
+Data['Vehicle_Med_Pay_Limit'].fillna((Data['Vehicle_Med_Pay_Limit'].mean()), inplace=True)
 
-Data.replace(replacements, regex=True, inplace=True)
-#Data['Policy_Zip_Code_Garaging_Location'] = Data['Policy_Zip_Code_Garaging_Location'].replace('Unknown', '00000')
+# Print unique values in each column #
+
 for col in Data[1:]:
     print Data[col].unique()
-    #print Data[col].columns
-#print Data[1].unique()
-#print list(Data.columns.values)
-#print(Data.head())
 
+# Save modified data to new csv file #
+
+Data.to_csv('new_data.csv', sep=',', encoding='utf-8')
