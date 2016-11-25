3bfa5c6ab731fb865baf13ed6c7df94835da7282
Cizhen Wu <wucizhen@gmail.com>
2016-11-25 11:54:49 -0500

    Update preprocess.py
    Classified Driver_Total_Ages_xxx to 3 categories: under 25, 25-64, over 64
    Dropped ALL Driver_Total_Ages_xxx columns.
    Dropped vehicle_make_description column.

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 
 
-Data = pd.read_csv('Training_Data_2016/training_data_2016.csv',low_memory = False)
+Data = pd.read_csv('training_data_2016.csv',low_memory = False)
 
 # Drop unneccessary columns #
 
@@ -17,6 +17,7 @@ Data = Data.drop('Driver_Minimum_Age', 1)
 Data = Data.drop('Driver_Maximum_Age', 1)
 Data = Data.drop('EEA_PolicyYear', 1)
 Data = Data.drop('Vehicle_New_Cost_Amount', 1)
+Data = Data.drop('vehicle_make_description', 1)
 
 # Clearing unneccessary rows #
 
@@ -40,11 +41,29 @@ Data['Vehicle_Collision_Coverage_Deductible'] = Data['Vehicle_Collision_Coverage
 Data['Vehicle_Miles_To_Work'].fillna((Data['Vehicle_Miles_To_Work'].mean()), inplace=True)
 Data['Vehicle_Med_Pay_Limit'].fillna((Data['Vehicle_Med_Pay_Limit'].mean()), inplace=True)
 
+# Classify driver_Age into 3 categories Cizhen #
+
+Data['Driver_Age_Under_25'] = pd.Series(Data.Driver_Total_Teenager_Age_15_19 + Data.Driver_Total_College_Ages_20_23, index=Data.index)
+Data['Driver_Age_25_To_64'] = pd.Series(Data.Driver_Total_Young_Adult_Ages_24_29 + Data.Driver_Total_Low_Middle_Adult_Ages_30_39 + Data.Driver_Total_Middle_Adult_Ages_40_49 + Data.Driver_Total_Adult_Ages_50_64, index=Data.index)
+Data['Driver_Age_Above_64'] = pd.Series(Data.Driver_Total_Senior_Ages_65_69 + Data.Driver_Total_Upper_Senior_Ages_70_plus, index=Data.index)
+
+# Drop the unclassified Driver_Ages_xxx columns Cizhen #
+
+Data = Data.drop('Driver_Total_Teenager_Age_15_19', 1)
+Data = Data.drop('Driver_Total_College_Ages_20_23', 1)
+Data = Data.drop('Driver_Total_Young_Adult_Ages_24_29', 1)
+Data = Data.drop('Driver_Total_Low_Middle_Adult_Ages_30_39', 1)
+Data = Data.drop('Driver_Total_Middle_Adult_Ages_40_49', 1)
+Data = Data.drop('Driver_Total_Adult_Ages_50_64', 1)
+Data = Data.drop('Driver_Total_Senior_Ages_65_69', 1)
+Data = Data.drop('Driver_Total_Upper_Senior_Ages_70_plus', 1)
+
 # Print unique values in each column #
 
-for col in Data[1:]:
-    print Data[col].unique()
+# for col in Data[1:]:
+#     print Data[col].unique()
 
 # Save modified data to new csv file #
 
 Data.to_csv('new_data.csv', sep=',', encoding='utf-8')
+print 'done'
